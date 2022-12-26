08645b14bdb06bfb9617e24ec86fd778114777fa
sagarj-dev <120121397+sagarj-dev@users.noreply.github.com>
2022-12-26 13:43:20 +0530

    first commit

--- popups/page2.js
+++ popups/page2.js
@@ -1,28 +1,25 @@
-document.addEventListener("DOMContentLoaded", async () => {
-  $("#selectpicker").selectpicker();
+$(function () {
+  $("#selectpicker").multiselect({
+    selectAllValue: "multiselect-all",
+    enableCaseInsensitiveFiltering: true,
+    enableFiltering: true,
+    maxHeight: "300",
+    buttonWidth: "235",
+    onChange: function (element, checked) {
+      var brands = $("#selectpicker option:selected");
+      var selected = [];
+      $(brands).each(function (index, brand) {
+        selected.push([$(this).val()]);
+      });
 
-  const { GOVUK_LOCATIONS } = await chrome.storage.local.get("GOVUK_LOCATIONS");
-
-  let select = document.getElementById("selectpicker");
-
-  select.addEventListener("change", (e) => {
-    console.log(select.value);
+      console.log(selected);
+    },
   });
 
-  //   $("#selectpicker").selectpicker({
-  //     selectAllValue: "multiselect-all",
-  //     enableCaseInsensitiveFiltering: true,
-  //     enableFiltering: true,
-  //     maxHeight: "300",
-  //     buttonWidth: "235",
-  //     onChange: function (element, checked) {
-  //       var brands = $("#selectpicker option:selected");
-  //       var selected = [];
-  //       $(brands).each(function (index, brand) {
-  //         selected.push([$(this).val()]);
-  //       });
-
-  //       console.log(selected);
-  //     },
-  //   });
+  //   const { GOVUK_LOCATIONS } = await chrome.storage.local.get("GOVUK_LOCATIONS");
+  var data = [
+    { label: "option1Label", value: "option1Value" },
+    { label: "option2Label", value: "option2Value" },
+  ];
+  $("#selectpicker").multiselect("dataprovider", data);
 });
